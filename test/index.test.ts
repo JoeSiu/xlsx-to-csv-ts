3ba4d17e4902763f8e4282b6256bb06a44fbc0fb
JoeSiu <joesiu.dev@gmail.com>
2024-02-22 00:59:12 +0800

    feat: update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -10,13 +10,44 @@ it("output csv", async () => {
   }).not.toThrowError();
 });
 
-it("output fitered csv", async () => {
+it("output fitered csv (all column)", async () => {
   expect(async () => {
     await convertXlsxToCsv({
       inputFile: "./public/sample.xlsx",
-      outputFilename: "sample-filtered",
+      outputFilename: "sample-filtered-all",
       outputDir: "./public/",
-      filter: { Segment: "segment" },
+      filter: {
+        Segment: "segment",
+        Country: "country",
+        Product: "product",
+        "Discount Band": "discount-band",
+        "Units Sold": "units-sold",
+        "Manufacturing Price": "manufacturing-price",
+        "Sale Price": "sale-price",
+        "Gross Sales": "gross-sale",
+        Discounts: "discounts",
+        Sales: "sales",
+        COGS: "cogs",
+        Profit: "profit",
+        Date: "date",
+        "Month Number": "month-number",
+        "Month Name": "month-name",
+        Year: "year",
+      },
+    });
+  }).not.toThrowError();
+});
+
+it("output fitered csv (selected column)", async () => {
+  expect(async () => {
+    await convertXlsxToCsv({
+      inputFile: "./public/sample.xlsx",
+      outputFilename: "sample-filtered-selected",
+      outputDir: "./public/",
+      filter: {
+        Product: "product",
+        Sales: "sales",
+      },
     });
   }).not.toThrowError();
 });
